72724b67640a2a5642aeb00a0b93a4bf62f29960
野澤悠 <nozawayuu@NozawaMac.local>
2020-07-18 12:18:30 +0900

    45.クラスでデータを作成するまで完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Comment } from './class/comment';
+import { User } from './class/user';
 
+const CURRENT_USER: User = new User(1, '松本人志');
+const ANOTHER_USER: User = new User(2, '浜田雅功');
+const COMMENTS: Comment[] = [
+    new Comment(ANOTHER_USER, 'うぅわ！'),
+    new Comment(ANOTHER_USER, 'じゅわあああぁぁああぁ！！'),
+    new Comment(CURRENT_USER, 'なははははははははは'),
+    new Comment(CURRENT_USER, 'ホンマに嫌になって帰るときのウルトラマンか？'),
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMMENTS;
+  currentUser = CURRENT_USER;
 }
