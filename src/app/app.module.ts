8351e594227e67c273edfc303eea7bc66965cbad
野澤悠 <nozawayuu@NozawaMac.local>
2020-07-25 15:41:15 +0900

    49.Pipeで日付を整形するまで完了

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { CommentDatePipe } from './pipe/comment-date.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentDatePipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
